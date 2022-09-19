7d52da76d27aaba4605f60b62f1d8e085a3b2474
q22tt <alenaartamonova066@gmail.com>
2022-09-19 16:02:45 +0300

    add second version

--- script.js
+++ script.js
@@ -32,3 +32,29 @@ console.log(loadGrain([2, 0, 1, 5, 2, 7]))
 console.log(loadGrain([2, 4, 2]))
 console.log(loadGrain([7, 4]))
 console.log(loadGrain([]))
+
+
+//my version 
+
+function loadGrain2(levels){
+    let result =0
+    let wall = 0
+    for(i=0; i < levels.length-1 ; i++ ){
+        if(levels[i]<wall ){
+                result += wall - levels[i]
+        }  else {
+            wall = levels[i]
+        }
+    }
+    return result
+}
+
+console.log(loadGrain2([4, 1, 3]))
+console.log(loadGrain2([2, 1, 5, 2, 7, 4, 10]))
+console.log(loadGrain2([2, 0, 1, 5, 2, 7]))
+console.log(loadGrain2([2, 4, 2]))
+console.log(loadGrain2([7, 4]))
+console.log(loadGrain2([]))
+
+
+
